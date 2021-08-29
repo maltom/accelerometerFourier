791d7a29ab74110c5502c3eaa4afd24816a50376
maltom <tomaszpmalachowski@gmail.com>
2021-08-30 00:48:03 +0200

    showing gui

--- src/mainWindow.java
+++ src/mainWindow.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class mainWindow {
+
+    public mainWindow()
+    {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,300,300));
+        panel.setLayout(new GridLayout(0,1));
+        frame.add(panel,BorderLayout.CENTER);
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        frame.setTitle("Accelerometer Fourier Analysis");
+        frame.pack();
+        frame.setVisible(true);
+    }
     public static void main(String[] args)
     {
-
+        new mainWindow();
     }
 }
