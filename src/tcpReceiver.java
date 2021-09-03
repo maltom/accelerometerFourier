d04eb325cff5f7e3ee80ae3a6568a477b5c483b3
maltom <tomaszpmalachowski@gmail.com>
2021-09-04 00:08:31 +0200

    tcp

--- src/tcpReceiver.java
+++ src/tcpReceiver.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class tcpReceiver {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class tcpReceiver
+{
+private Socket tcpSocket;
+private BufferedReader incomingMessage;
+private int port = 2134;
+private String ip = "192.168.1.11";
+public void startConnection() throws IOException {
+    tcpSocket = new Socket(ip,port);
+    incomingMessage = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+}
+public void stopConnection() throws IOException {
+    incomingMessage.close();
+    tcpSocket.close();
+}
 }
